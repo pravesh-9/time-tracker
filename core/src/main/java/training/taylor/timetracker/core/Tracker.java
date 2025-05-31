fe94aa02af4bd92163cfbd8f08859e674b787dc6
SONU KUMAR <sonuk668929@gmail.com>
2025-05-31 17:05:44 +0530

    Update Tracker.java
    UPDATED BY ME FIRST TIME 
    ADDED NEW LINE IN PREVIOUS CODE

--- core/src/main/java/training/taylor/timetracker/core/Tracker.java
+++ core/src/main/java/training/taylor/timetracker/core/Tracker.java
@@ -4,6 +4,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 import training.taylor.timetracker.core.dao.TimeEntry;
 
+//new line of code added by SONU KUMAR
+
 import java.util.List;
 
 @Component
